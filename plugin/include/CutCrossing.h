3ff51ebdbd3591d51774989ea1444086e6ea11fd
oshersh15 <osher.shechter@gmail.com>
2026-07-11 17:28:40 +0100

    Add CutCrossing data structure

---
+++ plugin/include/CutCrossing.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "HalfEdge.h"
+
+/*
+    Stores one detected crossing between a sampled profile curve
+    and an edge of the mesh.
+
+    The IDs identify the curve, sampled curve segment, mesh face,
+    and half-edge associated with the crossing.
+
+    The position is stored on the mesh edge because the eventual
+    cut belongs to the mesh surface.
+*/
+struct CutCrossing
+{
+    int curveId = -1;
+    int curveSegmentId = -1;
+    int faceId = -1;
+    int halfEdgeId = -1;
+
+    Point3 position;
+};
